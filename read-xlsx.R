a7a848052b6dcc76908e1f19c2b9d010ec4fb6fc
Shuai <sudapink@gmail.com>
2016-06-28 22:42:40 -0400

    Add match function

--- read-xlsx.R
+++ read-xlsx.R
@@ -1,5 +1,10 @@
-fileName <-"./Earlyemployee/2012 Banco Funcionarios Final 2012-05-24.xlsx"
+fileName <-"/Volumes/MANSOUR/FIA project/Earlyemployee/2012 Banco Funcionarios Final 2012-05-24.xlsx"
 library(openxlsx)
-data<-read.xlsx(fileName,colNames=T)
-firmName <-"Firm-2012-with-descriptions-Jun-22.xlsx"
+data<-read.xlsx(fileName,colNames=T,startRow=2)
+firmName <-"/Volumes/MANSOUR/FIA project/Firm-2012-with-descriptions-Jun-22.xlsx"
 firmData <-read.xlsx(firmName,colNames=T)
+findCNPJ <-function(x){
+  idx <- which(firmData["CODIGO"]==x)
+    return(firmData[idx,2])
+}
+CnpjCode<-data.frame(sapply(data[,1],findCNPJ))
