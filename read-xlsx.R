93689161c0bab309dd52fd5ebfc01df7e2610b9d
Shuai <sudapink@gmail.com>
2016-06-29 22:47:52 -0400

    Add function for matching cnpj code

--- read-xlsx.R
+++ read-xlsx.R
@@ -1,15 +1,14 @@
-fileName <-"/Volumes/MANSOUR/FIA project/Earlyemployee/2012 Banco Funcionarios Final 2012-05-24.xlsx"
+fileName <-"/Volumes/MANSOUR/FIA project/step 1/2013 employee.xlsx"
 library(openxlsx)
-data<-read.xlsx(fileName,colNames=T,startRow=2)
-firmName <-"/Volumes/MANSOUR/FIA project/Firm-2012-with-descriptions-Jun-22.xlsx"
+data<-read.xlsx(fileName,colNames=T)
+firmName <-"/Volumes/MANSOUR/FIA project/step 1/Firm-2013.xlsx"
 firmData <-read.xlsx(firmName,colNames=T)
 findCNPJ <-function(x){
   idx <- which(firmData["CODIGO"]==x)
-    return(as.numeric(firmData[idx,2]))
+    return(toString(firmData[idx,2]))
 }
-CnpjCode<-lapply(data[,1],findCNPJ)
+CnpjCode<-apply(data,1,findCNPJ)
 CnpjCode<-data.frame(t(CnpjCode))
 transformCnpj<-t(CnpjCode)
-transformCnpj<-as.numeric(transformCnpj)
 fullData<-data.frame(cbind(transformCnpj,data))
-write.xlsx(fullData,"/Volumes/MANSOUR/FIA project/2012-June-28-with-cnpj.xlsx")
+write.xlsx(fullData,"/Volumes/MANSOUR/FIA project/step 1/2013-June-29-with-cnpj.xlsx")
